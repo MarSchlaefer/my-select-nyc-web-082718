dbdb40388bfc3153573614d0cc089253588cc638
Mariel Schlaefer <mschlaefer00@gmail.com>
2018-08-10 18:07:43 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,14 @@
 def my_select(collection)
   i = 0 
+  selected_collection = []
   
   while i < collection.length 
     yield(collection[i])
     
+    if collection[i] === true
+      selected_collection.push(colllection[i])
+    end 
     i += 1 
   end 
-  collection
+  selected_collection
 end
